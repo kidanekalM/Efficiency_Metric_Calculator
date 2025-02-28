7006d70f9828cba437cd847f3f01368e4a724b6a
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2025-02-28 15:50:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
-document.getElementById('calculate').addEventListener('click', (e) => {
+let data = [];
 
+document.getElementById('calculate').addEventListener('click', (e) => {
     const name = document.getElementById('name').value;
-    const weight = document.getElementById('weight').value;
-    const cost = document.getElementById('cost').value;
-    const height = document.getElementById('height').value;
-    const diameter = document.getElementById('diameter').value;
+    const weight = parseFloat(document.getElementById('weight').value);
+    const cost = parseFloat(document.getElementById('cost').value);
+    const height = parseFloat(document.getElementById('height').value);
+    const diameter = parseFloat(document.getElementById('diameter').value);
     const volume = Math.PI * Math.pow(diameter / 2, 2) * height;
-    const efficiencyMetric = weight / (cost * volume);
-    efficiencyMetric = efficiencyMetric.toFixed(2);
-    const table = document.querySelector('table');
-    const row = document.createElement('tr');
-    row.innerHTML = `<td>${name}</td><td>${weight}</td><td>${cost}</td><td>${volume}</td><td>${efficiencyMetric}</td>`;
-    table.appendChild(row);
+    let efficiencyMetric = weight / (cost * volume);
+    console.log(efficiencyMetric);
+    // efficiencyMetric = efficiencyMetric.toFixed(2);
+    
+    data.push({name, weight, cost, volume, efficiencyMetric});
+    
     document.getElementById('name').value = '';
     document.getElementById('weight').value = '';
     document.getElementById('cost').value = '';
     document.getElementById('height').value = '';
     document.getElementById('diameter').value = '';
-})
+    console.log(data);
+
+    sort(data);
+    reloadTable(document.querySelector('table'), data);
+});
+
+function sort(data) {
+    return data.sort((a, b) => b.efficiencyMetric - a.efficiencyMetric);
+}
+
+function reloadTable(table, data) {
+    let header = table.firstElementChild;
+
+    // Clear the table except the header
+    while (table.firstChild) {
+        table.removeChild(table.firstChild);
+    }
+    table.appendChild(header);
+
+    // Iterate over the data array
+    data.forEach((datum) => {
+        console.log(datum);
+        const row = document.createElement('tr');
+        row.innerHTML = `<td>${datum.name}</td><td>${datum.weight}</td><td>${datum.cost}</td><td>${datum.volume}</td><td>${datum.efficiencyMetric}</td>`;
+        table.appendChild(row);
+    });
+}
